68f7315398a1da66ed06aaae758e0414cab182c0
Branomps <branomps@gmail.com>
2018-06-28 15:27:11 +0100

    Add Section class to Models.

---
+++ MissionIT.WebHelp/Models/Section.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MissionIT.WebHelp.Models
+{
+    public class Section
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        public int SectionID { get; set; }
+        [Required, StringLength(100), Display(Name = "Name")]
+        public string SectionName { get; set; }
+        public virtual ICollection<Article> Articles { get; set; }
+    }
+}
