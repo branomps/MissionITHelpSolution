008be9027fc23200d8151ac51966b74005a72050
Branomps <branomps@gmail.com>
2018-06-28 15:25:36 +0100

    Add  Article class to Models

---
+++ MissionIT.WebHelp/Models/Article.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MissionIT.WebHelp.Models
+{
+    public class Article
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        public int ArticleID { get; set; }
+        [Required, StringLength(100), Display(Name = "Name")]
+        public string ArticleName { get; set; }
+        [Required, StringLength(10000), Display(Name = "Article Description"), DataType(DataType.MultilineText)]
+        public string ArticleDescription { get; set; }
+        [Required]
+        public string ArticleImageUrl { get; set; }
+        public int? SectionID { get; set; }
+
+        public Section Section { get; set; }
+    }
+}
