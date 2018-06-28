8ef4b3d09de7568e61f849eb94bad17f81adb97a
Branomps <branomps@gmail.com>
2018-06-28 15:40:46 +0100

    Create HelpDbContext in Data.

---
+++ MissionIT.WebHelp/Data/HelpDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MissionIT.WebHelp.Models;
+
+namespace MissionIT.WebHelp.Data
+{
+    public class HelpDbContext : DbContext
+    {
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<Article> Articles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Section>().ToTable("tblSections");
+            modelBuilder.Entity<Article>().ToTable("tblArticles");
+        }
+    }
+}
