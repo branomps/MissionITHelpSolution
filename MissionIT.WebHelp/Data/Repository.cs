b7ebc26c9790e080a060b11d8d0637965401c6e9
Branomps <branomps@gmail.com>
2018-06-29 15:42:25 +0100

    Create and Implement Repository. Change IRepository.

---
+++ MissionIT.WebHelp/Data/Repository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using MissionIT.WebHelp.Models;
+
+namespace MissionIT.WebHelp.Data
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly  HelpDbContext Context;
+
+        public Repository(HelpDbContext ctx)
+        {
+            Context = ctx;
+        }
+
+        public T GetOne(int id)
+        {
+            return Context.Set<T>().Find(id);
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return Context.Set<T>().ToList();
+        }
+        public IQueryable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return Context.Set<T>().Where(predicate);
+        }
+
+        public void Add(T tEntity)
+        {
+            Context.Set<T>().Add(tEntity);
+        }
+
+        public void Update(T tEntity)
+        {
+            Context.Set<T>().Update(tEntity);
+        }
+
+        public void Remove(T tEntity)
+        {
+            Context.Set<T>().Remove(tEntity);
+        }
+    }
+}
--- MissionIT.WebHelp/Models/IDataRepository.cs
+++ MissionIT.WebHelp/Models/IDataRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace MissionIT.WebHelp.Models
@@ -8,11 +9,11 @@ namespace MissionIT.WebHelp.Models
     internal interface IRepository<T> where T : class
     {
         T GetOne(int id);
-        IQueryable<T> GetAll();
-        IQueryable<T> Find(string s);
+        IEnumerable<T> GetAll();
+        IQueryable<T> Find(Expression<Func<T, bool>> predicate);
         void Add(T tEntity);
-        void Update(T tEntiy);
-        void Remove(int id);
+        void Update(T tEntity);
+        void Remove(T tEntity);
 
     }
 }
