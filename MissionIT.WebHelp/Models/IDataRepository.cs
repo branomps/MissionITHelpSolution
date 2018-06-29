0ee3bce9c6c1b4034791e58f5337d95156be7590
Branomps <branomps@gmail.com>
2018-06-29 14:46:28 +0100

    Create IDataRepository with basic operations.

---
+++ MissionIT.WebHelp/Models/IDataRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MissionIT.WebHelp.Models
+{
+    internal interface IRepository<T> where T : class
+    {
+        T GetOne(int id);
+        IQueryable<T> GetAll();
+        IQueryable<T> Find(string s);
+        void Add(T tEntity);
+        void Update(T tEntiy);
+        void Remove(int id);
+
+    }
+}
