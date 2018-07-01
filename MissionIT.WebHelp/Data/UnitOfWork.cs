09867ed881c1c074b970cd3f5fc4ccf37f36a9ee
BranislavMacosinec <branomps@gmail.com>
2018-07-01 22:52:23 +0100

    Update UnitOfWork.

--- MissionIT.WebHelp/Data/UnitOfWork.cs
+++ MissionIT.WebHelp/Data/UnitOfWork.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MissionIT.WebHelp.Models;
+
 
 namespace MissionIT.WebHelp.Data
 {
     public class UnitOfWork
     {
+        private HelpDbContext _context;
+
+        
+        
+        public UnitOfWork(HelpDbContext context)
+        {
+            _context = context;
+            Sections = new SectionRepository(get; set;) 
+            Articles = new ArticleRepository { get; set; }
+        }
+
+        public ISectionRepository Sections { get; set; }
+        public IArticleRepository Articles { get; set; }
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
     }
 }
