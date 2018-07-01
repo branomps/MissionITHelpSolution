62be6af113f36deff804603f0bb13c1676a7b108
BranislavMacosinec <branomps@gmail.com>
2018-07-01 23:08:22 +0100

    Create SectionRepository.

---
+++ MissionIT.WebHelp/Data/SectionRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MissionIT.WebHelp.Models;
+
+namespace MissionIT.WebHelp.Data
+{
+    public class SectionRepository : Repository<Section>, ISectionRepository
+    {
+        public SectionRepository(HelpDbContext context)
+            : base(context)
+        {
+
+        }
+
+
+        public HelpDbContext HelpDbContext
+        {
+            get { return Context as HelpDbContext; }
+            set { HelpDbContext = value; }
+        }
+
+        public IEnumerable<Section> GetSectionsWithArticles()
+        {
+            return HelpDbContext.Sections.
+        }
+    }
+}
